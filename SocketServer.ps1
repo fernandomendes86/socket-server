0d7c6dd20b136162037aadeb581ead318b4bd82f
Fernando Mendes <fernando.msantos1@gmail.com>
2026-07-29 07:19:28 -0300

    Refatora string e remove comentários

--- SocketServer.ps1
+++ SocketServer.ps1
@@ -1,21 +1,3 @@
-<#
---Subindo o SocketServer--
-Exemplo 1:
-.\SocketServer.ps1
--Inicia o servidor em 0.0.0.0 (todas as interfaces de rede [MEU_IP_DE_REDE]) na porta padrão e com resposta padrão.
-
--Para testar uma conexão remota, utilize o Telnet ou o PuTTY (em modo Raw), por exemplo, no telnet:
-> telnet MEU_IP_DE_REDE 5000
-
-Exemplo 2:
-.\SocketServer.ps1 -Porta 5001
--Inicia o servidor em  0.0.0.0 (todas as interfaces de rede [MEU_IP_DE_REDE]) na porta 5001 e com resposta padrão.
-
-Exemplo 3:
-.\SocketServer.ps1 -Ip "127.0.0.1" -Porta 5001 -Resposta "RESPOSTA_DO_TESTE_1234"
--Inicia o servidor localmente na porta 5001 e com resposta personalizada.
-#>
-
 param (
     [string]$Ip = "0.0.0.0",
     [int]$Porta = 5000,
@@ -45,6 +27,9 @@ try {
         if ($bytes -gt 0)
         {
             $recebido = [System.Text.Encoding]::ASCII.GetString($buffer, 0, $bytes)
+            $recebido = [Regex]::Replace($recebido, "\r?\n", "")
+            if (-not $recebido.Trim()) { break }
+
             Write-Host "Recebido: $recebido"
 
             $dados = [System.Text.Encoding]::ASCII.GetBytes($Resposta)
